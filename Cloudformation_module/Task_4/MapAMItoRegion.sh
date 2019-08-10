e7e607bc931562356f62c4a2dff2255a9e6bc13c
Dmitriy Kravets <Dmitriy_Kravets@epam.com>
2019-08-10 10:35:23 +0600

    separated folders

--- Cloudformation_module/Task_4/MapAMItoRegion.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-echo "Searching for image AmazonLinux2"
-regions=$(aws ec2 describe-regions --output text --query 'Regions[*].RegionName')
-for region in $regions; do
-    (
-     echo "$region $(  aws ec2 describe-images --region $region --owners amazon --filters 'Name=name,Values=amzn2-ami-hvm-2.0.????????-x86_64-gp2' 'Name=state,Values=available' --output json | jq -r '.Images | sort_by(.CreationDate) | last(.[]).ImageId')"
-    )
-done
