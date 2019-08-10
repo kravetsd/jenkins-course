2c44970b110d49363cc2d9fe8143ba1fdaaa6f65
Dmitriy Kravets <Dmitriy_Kravets@epam.com>
2019-08-10 11:27:40 +0600

    fixed repos in dsl job

--- Jenkins_module/job-dsl/nodejs.groovy
+++ Jenkins_module/job-dsl/nodejs.groovy
@@ -1,8 +1,8 @@
 job('NodeJS example') {
     scm {
-        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+        git('git://github.com/kravetsd/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+            node / gitConfigEmail('jenkins-dsl@example.com')
         }
     }
     triggers {
